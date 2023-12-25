3ac1b5798852ffad178b0f1ddbd0e89275361303
ABDULRAFEH12 <152694860+ABDULRAFEH12@users.noreply.github.com>
2023-12-25 14:10:44 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Calculator from './Components/Calculator'
+// import myImages from './Components/img1.png'
+function App() {
+  return (
+   <Calculator/>
+  //  <myImages/>
+  );
+}
+
+export default App;
